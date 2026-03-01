6b1c537c5ecb5694a7ac07e2d7151a43a1440394
Ulisses Gomes Oliveira <gomesoliveiraulissesri@gmail.com>
2026-02-28 22:41:59 -0300

    Primeiro passo: fixação da base 'datario' para visualização das tabelas reuqisitadas
    Added comments explaining the purpose and approach for analyzing SQL questions.

--- analise_sql.sql
+++ analise_sql.sql
@@ -1,1 +1,7 @@
 #Arquivo criado para hospedar os códigos e respostas das perguntas 1 a 10 do arquivo "perguntas_sql.md" em SQL.
+  
+*/Para conseguir visualizar a estrutura das tabelas do desafio, achei melhor adotar como primeiro passo
+a fixação da base de dados 'datario' ao meu painel lateral no BigQuery para visualizar a disposição das tabelas requisitadas,
+achei um caminho mais adequado que usar uma consulta com o operador LIMIT pois assim não consumiria minha cota gratuita de consultas.*/
+
+
